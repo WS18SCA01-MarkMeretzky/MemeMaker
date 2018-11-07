0b44c4559fbdd5668e986d69bd1475e646f9bcf2
Mark Meretzky <mark.meretzky@gmail.com>
2018-11-06 20:10:35 -0500

    Commit.

---
+++ MemeMaker/CaptionChoice.swift
@@ -0,0 +1,14 @@
+//
+//  CaptionChoice.swift
+//  MemeMaker
+//
+//  Created by Mark Meretzky on 11/6/18.
+//  Copyright © 2018 New York University School of Professional Studies. All rights reserved.
+//
+
+import Foundation;
+
+struct CaptionOption {
+    let emoji: String;
+    let caption: String;
+};
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -6,15 +6,53 @@
 //  Copyright © 2018 New York University School of Professional Studies. All rights reserved.
 //
 
-import UIKit
+import UIKit;
 
 class ViewController: UIViewController {
+    
+    var topChoices: [CaptionOption] = [
+        CaptionOption(emoji: "🕶", caption: "You know what's cool?"),
+        CaptionOption(emoji: "💥", caption: "You know what makes me mad?"),
+        CaptionOption(emoji: "💕", caption: "You know whatI love?")
+    ];
+    
+    var bottomChoices: [CaptionOption] = [
+        CaptionOption(emoji: "🐱", caption: "Cats wearing hats"),
+        CaptionOption(emoji: "🐕", caption: "Dogs carrying logs"),
+        CaptionOption(emoji: "🐒", caption: "Monkeys being funky")
+    ];
 
+    @IBOutlet weak var topCaptionSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var bottomCaptionSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var topCaptionLabel: UILabel!
+    @IBOutlet weak var bottomCaptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        topCaptionSegmentedControl.removeAllSegments();
+        for choice in topChoices {
+            topCaptionSegmentedControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false);
+        }
+        topCaptionSegmentedControl.selectedSegmentIndex = 0;
+        
+        bottomCaptionSegmentedControl.removeAllSegments();
+        for choice in bottomChoices {
+            bottomCaptionSegmentedControl.insertSegment(withTitle: choice.emoji, at: bottomChoices.count, animated: false);
+        }
+        bottomCaptionSegmentedControl.selectedSegmentIndex = 0;
+        
+        setLabels();
     }
-
-
+    
+    @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        setLabels();
+    }
+    
+    func setLabels() {
+        topCaptionLabel.text = topChoices[topCaptionSegmentedControl.selectedSegmentIndex].caption;
+        bottomCaptionLabel.text = bottomChoices[bottomCaptionSegmentedControl.selectedSegmentIndex].caption;
+    }
+    
 }
-
